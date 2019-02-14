8584450d1b6fcca5b4b03eee1a614e5aaecb0ada
leonzwakenberg <46625592+leonzwakenberg@users.noreply.github.com>
2019-02-14 12:55:51 +0100

    aanmaken

---
+++ src/main/java/com/schaaktoernooi/schaaktoernooi/SchaaktoernooiApplication.java
@@ -0,0 +1,14 @@
+package com.schaaktoernooi.schaaktoernooi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaaktoernooiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaaktoernooiApplication.class, args);
+	}
+
+}
+
